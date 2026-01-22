0478b5c859fa5fb5b1c9f899901af984e0d77c14
sangmin08 <big1024565@naver.com>
2026-01-22 10:40:11 +0900

    Update script.js

--- 2주차 과제 JS/0122/script.js
+++ 2주차 과제 JS/0122/script.js
@@ -13,7 +13,8 @@ function insertElement(arr, index, value){
     if (arr.length < index) {
         console.error(`현재 배열의 길이는 ${arr.length}로 ${index}은 입력 불가능합니다.`)
     }else {
-        console.log(arr.splice(index, 1, value))
+        arr.splice(index, 0, value)
+        console.log(arr)
     }
 }
 
@@ -21,3 +22,16 @@ const nums = [1,2,3,4,5];
 
 console.log(insertElement(nums, 2, 6));
 console.log(insertElement(nums, 10, 6));
+
+// 3번
+function removeElement(arr, index) {
+    arr.splice(index,1)
+    console.log(arr)
+}
+
+const chars = ['A','B','C','D','E'];
+
+console.log(removeElement(chars, 3));
+
+// 4번 
+function removeElement(arr, character)
