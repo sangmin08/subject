41608ef9e01b3fea642831bc6ee5b6b70e91cb45
sangmin08 <big1024565@naver.com>
2026-01-26 14:31:04 +0900

    3번

--- 3주차 과제 - 1/0126/3/main.js
+++ 3주차 과제 - 1/0126/3/main.js
@@ -1,11 +1,19 @@
+const btn1 = document.querySelector("#btn1")
+const btn2 = document.querySelector("#btn2")
+const btn3 = document.querySelector("#btn3")
+const area = document.querySelector("#displayArea")
+btn1.addEventListener('click',onClick1)
+btn2.addEventListener('click',onClick2)
+btn3.addEventListener('click',onClick3)
+
 function onClick1() {
-    
+    area.textContent = "버튼1"
 }
 
 function onClick2() {
-    
+    area.textContent = "버튼2"
 }
 
 function onClick3() {
-    
+    area.textContent = "버튼3"
 }
